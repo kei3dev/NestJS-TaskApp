49042d02ab851a2c61daf84a447add4d4aa3e14c
keisuke-333 <keikubo.999@gmail.com>
2024-05-25 13:04:35 +0900

    fix task resolver

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Task } from './models/task.model';
+import { Task } from '@prisma/client';
+import { Task as TaskModel } from './models/task.model';
 import { TaskService } from './task.service';
 import { CreateTaskInput } from './dto/createTask.input';
 
@@ -7,13 +8,15 @@ import { CreateTaskInput } from './dto/createTask.input';
 export class TaskResolver {
   constructor(private readonly taskService: TaskService) {}
 
-  @Query(() => [Task], { nullable: 'items' })
-  getTasks(): Task[] {
-    return this.taskService.getTasks();
+  @Query(() => [TaskModel], { nullable: 'items' })
+  async getTasks(): Promise<Task[]> {
+    return await this.taskService.getTasks();
   }
 
-  @Mutation(() => Task)
-  createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
-    return this.taskService.createTask(createTaskInput);
+  @Mutation(() => TaskModel)
+  async createTask(
+    @Args('createTaskInput') createTaskInput: CreateTaskInput,
+  ): Promise<Task> {
+    return await this.taskService.createTask(createTaskInput);
   }
 }
