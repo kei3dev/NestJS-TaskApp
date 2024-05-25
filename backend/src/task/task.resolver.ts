ea50b705c6bb60633414ee438d05548484b9fe1e
keisuke-333 <keikubo.999@gmail.com>
2024-05-25 14:47:51 +0900

    add update delete task resolver

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -1,8 +1,9 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Task } from '@prisma/client';
 import { Task as TaskModel } from './models/task.model';
 import { TaskService } from './task.service';
 import { CreateTaskInput } from './dto/createTask.input';
+import { UpdateTaskInput } from './dto/updateTask.input';
 
 @Resolver()
 export class TaskResolver {
@@ -18,5 +19,17 @@ export class TaskResolver {
     @Args('createTaskInput') createTaskInput: CreateTaskInput,
   ): Promise<Task> {
     return await this.taskService.createTask(createTaskInput);
+  }
+
+  @Mutation(() => TaskModel)
+  async updateTask(
+    @Args('updateTaskInput') updateTaskInput: UpdateTaskInput,
+  ): Promise<Task> {
+    return await this.taskService.updateTask(updateTaskInput);
+  }
+
+  @Mutation(() => TaskModel)
+  async deleteTask(@Args('id', { type: () => Int }) id: number): Promise<Task> {
+    return await this.taskService.deleteTask(id);
   }
 }
