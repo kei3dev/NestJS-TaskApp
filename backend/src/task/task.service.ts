83f5a5c2d801e84eabdf88d0fb15ccff9f1b9e9c
keisuke-333 <keikubo.999@gmail.com>
2024-05-27 01:31:53 +0900

    add userId for task service

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -7,14 +7,15 @@ import { UpdateTaskInput } from './dto/updateTask.input';
 @Injectable()
 export class TaskService {
   constructor(private readonly prismaService: PrismaService) {}
-  async getTasks(): Promise<Task[]> {
-    return await this.prismaService.task.findMany();
+
+  async getTasks(userId: number): Promise<Task[]> {
+    return await this.prismaService.task.findMany({ where: { userId } });
   }
 
   async createTask(createTaskInput: CreateTaskInput): Promise<Task> {
-    const { name, dueDate, description } = createTaskInput;
+    const { name, dueDate, description, userId } = createTaskInput;
     return this.prismaService.task.create({
-      data: { name, dueDate, description },
+      data: { name, dueDate, description, userId },
     });
   }
 
